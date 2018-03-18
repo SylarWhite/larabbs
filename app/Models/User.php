91b6fbe8d7c0671b44b97c21243383ddaddd3d21
Sylar.bigb <28239337@qq.com>
2018-03-18 05:48:21 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function topics()
+    {
+        // 一个用户有多条文章
+        return $this->hasMany(Topic::class);
+    }
 }
